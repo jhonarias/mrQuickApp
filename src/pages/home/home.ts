c40e0074cffef1b162f41366cf0d003974966f5d
jhonfreddy130 <john.arias@newshore.es>
2018-07-22 17:39:25 -0500

    Lista de lugares y slider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { ListPlacesPage } from '../list-places/list-places';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  listPlacepage: any;
   constructor(public navCtrl: NavController) {
-
+    this.listPlacepage = ListPlacesPage;
   }
 
 }
